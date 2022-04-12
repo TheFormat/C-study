abf0a993186dfbe812b3ae00738d2e2ca449ac7b
TheFormat <95959211+TheFormat@users.noreply.github.com>
2022-04-12 20:55:29 +0900

    Update training.cpp

--- training.cpp
+++ training.cpp
@@ -6,31 +6,45 @@ using namespace std;
 class BStat {
     
     private : 
-    int ctt = 0;
-    int pow = 0;
-    int eye = 0;
-    int spd = 0;
-    int fcs = 0;
-    int dfd = 0;
+        int contact = 30;
+        int power = 30;
+        int eye = 30;
+        int speed = 30;
+        int focus = 30;
+        int defend = 30;
     
     public :
-    void Contact() {
-        this -> ctt += 1;
-        this -> eye += 1;
+            // 선수에게 부여할 총 능력치 n. 6개 능력치에 랜덤으로 배분.contact에 (0,n)에서 무작의 난수 넣고, n-=난수 하고 power에 하고..반복
+        void First(int n) {
+            random_device rd;
+            mt19937 gen(rd());
+            uniform_int_distribution<int> dis(0,n);
+            this -> contact += dis(gen);
+            n -= dis(gen);
+        }
         
-    }
-    void Muscle() {
-        this -> pow += 1;
-    }
-    void Medit() {
-        this -> fcs += 1;
-    }
-    void Fungo() {
-        this -> dfd += 2;
-    }
-    void Exam() {
-        cout << "contact : " << ctt << endl;
-    }
+        void Contact() {
+            this -> contact += 1;
+            this -> eye += 1;
+        
+        }
+        void Muscle() {
+            this -> power += 1;
+        }
+        void Medit() {
+            this -> focus += 1;
+        }
+        void Fungo() {
+            this -> defend += 2;
+        }
+        void Rating() {
+            cout << "contact : " << contact << endl;
+            cout << "power : " << power << endl;
+            cout << "eye : " << eye << endl;
+            cout << "speed : " << speed<< endl;
+            cout << "focus : " << focus << endl;
+            cout << "defend : " << defend << endl;
+        }
 };
 
 
@@ -38,9 +52,10 @@ class BStat {
 
 int main() {
     BStat Alex;
-    Alex.Contact();
-    Alex.Exam();
-    
+    Alex.First(10);
+    Alex.Rating();
+    Alex.Fungo();
+    Alex.Rating();
     
     return 0;
 }
